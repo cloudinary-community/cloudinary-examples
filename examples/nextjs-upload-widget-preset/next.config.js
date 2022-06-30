e73fbb3c10a366409eaa297324289e18ee8131f0
colbyfayock <fayock@gmail.com>
2022-06-30 11:38:52 +0200

    cleanup

--- examples/nextjs-upload-widget-preset/next.config.js
+++ examples/nextjs-upload-widget-preset/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['res.cloudinary.com']
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
