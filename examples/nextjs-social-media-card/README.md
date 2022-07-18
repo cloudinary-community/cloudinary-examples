c0b690c9706252143b31b119d91d2d999bf533c3
colbyfayock <fayock@gmail.com>
2022-07-18 15:29:20 -0400

    social media card

---
+++ examples/nextjs-social-media-card/README.md
@@ -0,0 +1,25 @@
+# Next.js Social Media Card with Cloudinary
+
+Programmatically create an open graph social media card with dynamic text using Cloudinary.
+
+View Demo: <https://cloudinary-nextjs-social-media-card.netlify.app>
+
+## 🚀 Get Started with This Example
+
+* Install the project dependencies with:
+
+```
+yarn install
+# or
+npm install
+```
+
+* Start the development server with:
+
+```
+yarn dev
+# or
+npm run dev
+```
+
+* Visit the project at <http://localhost:3000>!
---
+++ examples/nextjs-social-media-card/pages/index.js
@@ -0,0 +1,111 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+/* The Node SDK will only work in node-based environments such as getStaticProps */
+import { v2 as cloudinary } from 'cloudinary';
+
+import styles from '../styles/Home.module.css';
+
+export default function Home({ ogImage }) {
+  return (
+    <>
+      <Head>
+        <title>Next.js Image Component &amp; Cloudinary</title>
+        <meta name="description" content="Find more Cloudinary examples at github.com/colbyfayock/cloudinary-examples" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content={ogImage} />
+        <meta property="og:image:secure_url" content={ogImage} />
+        <meta property="og:image:width" content="2024" />
+        <meta property="og:image:height" content="1012" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" content={ogImage} />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>
+            Next.js Social Media Card with Cloudinary
+          </h1>
+        </div>
+
+        <div className={styles.container}>
+          <h2>Card Preview</h2>
+          <img src={ogImage} />
+        </div>
+      </main>
+    </>
+  )
+}
+
+
+export async function getStaticProps() {
+  cloudinary.config({
+    cloud_name: 'colbycloud-examples'
+  });
+
+  const ogImage = cloudinary.url('social-card-mountain', {
+    width: 1012,
+    height: 506,
+    transformation: [
+      {
+        fetch_format: 'auto',
+        quality: 'auto'
+      },
+
+      // Begin Parent Text Overlay
+
+      {
+        color: '#2E2F38',
+        crop: 'fit',
+        width: 892,
+        overlay: {
+          font_family: 'Source Sans Pro',
+          font_size: 60,
+          font_weight: 'bold',
+          text: 'My Social Card'
+        },
+      },
+
+        // By nesting overlays, we can take advantage of the natural flow of the image
+        // or text in the overlay to position elements, such as setting a subtitle
+        // to dynamically arrange itself to follow a headline.
+
+        // Begin Nested Text Overlay
+
+        {
+          color: '#717387',
+          crop: 'fit',
+          width: 892,
+          overlay: {
+            font_family: 'Source Sans Pro',
+            font_size: 40,
+            font_weight: 'semibold',
+            text: 'This is an example of how to generate a social media card on the fly with Cloudinary.'
+          },
+        },
+
+        // End Nested Text Overlay
+
+        {
+          flags: 'layer_apply',
+          gravity: 'north_west',
+          y: 'h + 30'
+        },
+
+      // End  Parent Text Overlay
+
+      {
+        flags: 'layer_apply',
+        gravity: 'north_west',
+        x: 60,
+        y: 60
+      }
+    ]
+  });
+
+  return {
+    props: {
+      ogImage
+    }
+  }
+}
