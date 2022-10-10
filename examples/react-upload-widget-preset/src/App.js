3ae5a705ddde70c70732606cd23939fc18fe0384
colbyfayock <fayock@gmail.com>
2022-10-10 16:41:52 -0400

    upload widget in React

---
+++ examples/react-upload-widget-preset/src/App.js
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+
+import UploadWidget from './components/UploadWidget';
+
+import './App.css';
+
+function App() {
+  const [url, updateUrl] = useState();
+  const [error, updateError] = useState();
+
+  /**
+   * handleOnUpload
+   */
+
+  function handleOnUpload(error, result, widget) {
+    if ( error ) {
+      updateError(error);
+      widget.close({
+        quiet: true
+      });
+      return;
+    }
+    updateUrl(result?.info?.secure_url);
+  }
+
+  return (
+    <main className="main">
+      <div className="container">
+        <h1 className="title">
+          React &amp; Cloudinary Upload Widget
+        </h1>
+      </div>
+
+      <div className="container">
+        <h2>Unsigned with Upload Preset</h2>
+        <UploadWidget onUpload={handleOnUpload}>
+          {({ open }) => {
+            function handleOnClick(e) {
+              e.preventDefault();
+              open();
+            }
+            return (
+              <button onClick={handleOnClick}>
+                Upload an Image
+              </button>
+            )
+          }}
+        </UploadWidget>
+
+        {error && <p>{ error }</p>}
+
+        {url && (
+          <>
+            <p><img src={ url } alt="Uploaded image" /></p>
+            <p>{ url }</p>
+          </>
+        )}
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ examples/react-upload-widget-preset/src/components/UploadWidget/UploadWidget.js
@@ -0,0 +1,61 @@
+import { useEffect, useRef } from 'react';
+
+const UploadWidget = ({ children, onUpload }) => {
+  const cloudinary = useRef();
+  const widget = useRef();
+
+  // Store the Cloudinary window instance to a ref when the page renders
+
+  useEffect(() => {
+    cloudinary.current = window.cloudinary;
+  }, []);
+
+  /**
+   * createWidget
+   * @description Creates a new instance of the Cloudinary widget and stores in a ref
+   */
+
+  function createWidget() {
+    // Providing only a Cloud Name along with an Upload Preset allows you to use the
+    // widget without requiring an API Key or Secret. This however allows for
+    // "unsigned" uploads which may allow for more usage than intended. Read more
+    // about unsigned uploads at: https://cloudinary.com/documentation/upload_images#unsigned_upload
+
+    const options = {
+      cloudName: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME, // Ex: mycloudname
+      uploadPreset: process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET, // Ex: myuploadpreset
+    }
+
+    return cloudinary.current?.createUploadWidget(options,
+      function (error, result) {
+        // The callback is a bit more chatty than failed or success so
+        // only trigger when one of those are the case. You can additionally
+        // create a separate handler such as onEvent and trigger it on
+        // ever occurance
+        if ( error || result.event === 'success' ) {
+          onUpload(error, result, widget?.current);
+        }
+      }
+    );
+  }
+
+  /**
+   * open
+   * @description When triggered, uses the current widget instance to open the upload modal
+   */
+
+  function open() {
+    if ( !widget?.current ) {
+      widget.current = createWidget();
+    }
+    widget?.current && widget.current.open();
+  }
+
+  return (
+    <>
+      {children({ cloudinary: cloudinary.current, widget: widget.current, open })}
+    </>
+  )
+}
+
+export default UploadWidget;
