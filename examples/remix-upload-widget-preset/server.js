5be40bd89e13e67a6183e68da0aaaf8829ea51fe
colbyfayock <fayock@gmail.com>
2023-01-17 15:56:14 -0500

    netlifY

---
+++ examples/remix-upload-widget-preset/server.js
@@ -0,0 +1,6 @@
+import { createRequestHandler } from "@remix-run/netlify";
+import * as build from "@remix-run/dev/server-build";
+export const handler = createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+});
