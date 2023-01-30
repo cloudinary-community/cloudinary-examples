f4e231ab4efb5595c4382ea4f8830bf70bd55684
colbyfayock <fayock@gmail.com>
2023-01-30 14:42:28 -0300

    vue view

---
+++ examples/vue-advanced-image/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import CloudImage from "./components/CloudImage.vue";
+</script>
+
+<template>
+  <CloudImage imageName="examples/beach_udmhbt.jpg" />
+</template>
+
+<style scoped>
+</style>
---
+++ examples/vue-advanced-image/src/components/CloudImage.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { AdvancedImage } from "@cloudinary/vue";
+import { Cloudinary } from "@cloudinary/url-gen";
+
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
+  },
+});
+
+console.log('import.meta.env.VITE_CLOUDINARY_CLOUD_NAME', import.meta.env.VITE_CLOUDINARY_CLOUD_NAME)
+
+const props = defineProps({
+  imageName: String,
+});
+
+const myImg = cld.image(props.imageName);
+</script>
+
+<template>
+  <AdvancedImage :cldImg="myImg" />
+</template>
