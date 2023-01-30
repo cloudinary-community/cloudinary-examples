0c53c115f37f43e8afd8a55bf72970d662a62b8a
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-01-30 16:00:02 +0000

    Adds the Vue SDK example.

---
+++ examples/vue-use-sdk/src/components/CloudImage.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { AdvancedImage } from "@cloudinary/vue";
+import { Cloudinary } from "@cloudinary/url-gen";
+
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: "demo",
+  },
+});
+
+const props = defineProps({
+  imageName: String,
+});
+
+const myImg = cld.image(props.imageName);
+</script>
+
+<template>
+  <AdvancedImage :cldImg="myImg" />
+</template>
