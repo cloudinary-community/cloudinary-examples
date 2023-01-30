1fe86a58adf98464b6a8bfb94fff298c85fbc449
Colby Fayock <colbyfayock@users.noreply.github.com>
2023-01-30 14:22:22 -0500

    Update remix.config.js

--- examples/remix-upload-widget-preset/remix.config.js
+++ examples/remix-upload-widget-preset/remix.config.js
@@ -1,9 +1,13 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   serverBuildTarget: "netlify",
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.js"
+      : undefined,
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
+  // serverBuildPath: ".netlify/functions-internal/server.js",
   // publicPath: "/build/",
 };
