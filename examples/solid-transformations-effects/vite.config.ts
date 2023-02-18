279f0afc49233a7e283c6f1a188c833d75f66893
colbyfayock <fayock@gmail.com>
2023-02-17 21:45:35 -0500

    netlify adapter

--- examples/solid-transformations-effects/vite.config.ts
+++ examples/solid-transformations-effects/vite.config.ts
@@ -1,6 +1,7 @@
 import solid from "solid-start/vite";
+import netlify from "solid-start-netlify";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid({ adapter: netlify() })],
 });
