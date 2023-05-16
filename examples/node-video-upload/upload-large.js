1276ddf544805ee195407200935486ad69488fe2
Matias Hernandez <matiasfh@gmail.com>
2023-05-15 23:04:07 -0400

    split the script into upload and upload_large usage

---
+++ examples/node-video-upload/upload-large.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+async function run() {
+  const file = "./large.mp4";
+  try {
+    const result = await new Promise((resolve, reject) => {
+      cloudinary.uploader.upload_large(
+        file,
+        {
+          resource_type: "video",
+        },
+        (error, result) => {
+          if (error) {
+            reject(error);
+          }
+          resolve(result);
+        }
+      );
+    });
+    console.log(`> Result: ${result.secure_url}`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run();
