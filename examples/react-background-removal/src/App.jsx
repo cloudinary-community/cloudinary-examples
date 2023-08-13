8b97741252956b25a239d6c4ca6dcdbcdffd3c33
Matias Hernandez <matiasfh@gmail.com>
2023-08-13 10:13:19 -0400

    feat: simiplify version

--- examples/react-background-removal/src/App.jsx
+++ examples/react-background-removal/src/App.jsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 
 
 import { backgroundRemoval, dropShadow } from "@cloudinary/url-gen/actions/effect";
-import { scale } from "@cloudinary/url-gen/actions/resize";
+import { scale, fill} from "@cloudinary/url-gen/actions/resize";
 import { Cloudinary } from "@cloudinary/url-gen";
 
 // Create a Cloudinary instance and set your cloud name.
@@ -13,109 +13,43 @@ const cld = new Cloudinary({
   }
 });
 
-function getOriginalUrl(publicID, width, height) {
 
-  // Scale the image to the specified dimensions
-  // and optimize format and quality
-  const myImage = cld.image(publicID)
-    .resize(scale().width(width).height(height))
-    .format('auto')
-    .quality('auto');
-
-  // Return the URL of the image
-  return myImage.toURL();
-}
-
-function getBackgroundRemovedUrl(publicID, width, height) {
-
-  const myImage = cld.image(publicID);
-
-  // Apply the background removal effect, scale to the specified
-  // dimensions and optimize format and quality
-  myImage
-    .effect(backgroundRemoval())
-    .resize(scale().width(width).height(height))
-    .format('auto')
-    .quality('auto');
-
-  // Return the URL of the image
-  return myImage.toURL();
-}
-
-
-function getBackgroundRemovedShadowUrl(publicID, width, height, az = 215, el = 45, spr = 50) {
-
-  const myImage = cld.image(publicID);
-
-  // Apply the background removal and drop shadow effects,
-  // then scale the image to the specified dimensions
-  // and optimize format and quality
-  myImage
-    .effect(backgroundRemoval())
-    .effect(dropShadow().azimuth(az).elevation(el).spread(spr))
-    .resize(scale().width(width).height(height))
-    .format('auto')
-    .quality('auto');
-
-  // Return the URL of the image
-  return myImage.toURL();
-}
-
-function CldImage({ src }) {
-  return (
-    <img src={src} loading="lazy" />
-  )
-}
-
-function Album({ images, displayType = "original" }) {
-  let getUrl = getOriginalUrl
-  if(displayType === 'no-background') {
-    getUrl = getBackgroundRemovedUrl
-  }
-  if(displayType === 'no-background-shadow') {
-    getUrl = getBackgroundRemovedShadowUrl
-  }
-  return (
-    <div className="grid grid-cols-3 gap-2">
-      {images.map(image => <CldImage src={getUrl(image, 1920, 1278)} key={image} />)}
-    </div>
-  )
-}
 
 function App() {
   const { cars, furniture, chairs } = images
   const [collection, setCollection] = React.useState({ title: 'cars', images: cars })
-  const [display, setDisplay] = React.useState('original')
+
+
+  function getUrl(publicId) {
+    return cld.image(publicId)
+      .effect(backgroundRemoval()) // remove background
+      .effect(dropShadow()) // add drop shadow
+      .resize(scale().width(450).height(300))
+      .format('auto')
+      .quality('auto')
+      .toURL()
+  }
 
   return (
     <div className="max-w-3xl mx-auto py-20">
       <h2 className="text-3xl mx-auto">Product Selection</h2>
-      <div className="grid grid-cols-3" >
+      <div className="grid grid-cols-3 mt-5" >
         <label className="text-2xl">Cars
-          <input type="radio" name="collection" value="cars" onChange={e => setCollection({ title: 'cars', images: cars})} checked={collection.title === "cars"} />
+          <input type="radio" name="collection" value="cars" onChange={e => setCollection({ title: 'cars', images: cars })} checked={collection.title === "cars"} />
         </label>
         <label className="text-2xl">Furniture
-          <input type="radio" name="collection" value="furniture" onChange={e => setCollection({title: 'furniture', images: furniture})} checked={collection.title === "furniture"} />
+          <input type="radio" name="collection" value="furniture" onChange={e => setCollection({ title: 'furniture', images: furniture })} checked={collection.title === "furniture"} />
         </label>
         <label className="text-2xl">Chairs
-          <input type="radio" name="collection" value="chairs" onChange={e => setCollection({ title: 'chairs', images: chairs})} checked={collection.title === "chairs"} />
+          <input type="radio" name="collection" value="chairs" onChange={e => setCollection({ title: 'chairs', images: chairs })} checked={collection.title === "chairs"} />
         </label>
       </div>
-      <h2 className="text-3xl mx-auto">How to display Selection</h2>
-      <div className="grid grid-cols-3" >
-        <label className="text-2xl">Original
-          <input type="radio" name="display" value="original" onChange={e => setDisplay('original')} checked={display === "original"} />
-        </label>
-        <label className="text-2xl">No Background
-          <input type="radio" name="display" value="no-background" onChange={e => setDisplay('no-background')} checked={display === "no-background"} />
-        </label>
-        <label className="text-2xl">No Background + Shadow
-          <input type="radio" name="display" value="no-background-shadow" onChange={e => setDisplay('no-background-shadow')} checked={display === "no-background-shadow"} />
-        </label>
+      <div className="grid grid-cols-3 gap-4 mt-10">
+        {collection.images.map(image => <img src={getUrl(image)} key={image} />)}
       </div>
-      <Album images={collection.images} displayType={display}/>
     </div>
   )
 }
 
 export default App
+
