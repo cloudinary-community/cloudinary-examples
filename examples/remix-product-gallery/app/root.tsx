a047bbe02e5ab123f89decf903766d6720291f92
Kobe Ruado <insidiae423@gmail.com>
2023-10-03 23:32:36 +0800

    Add Remix Product Gallery example

---
+++ examples/remix-product-gallery/app/root.tsx
@@ -0,0 +1,36 @@
+import type { LinksFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import styles from "~/styles/index.css";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <script
+          src="https://product-gallery.cloudinary.com/all.js"
+          type="text/javascript"
+        ></script>
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
