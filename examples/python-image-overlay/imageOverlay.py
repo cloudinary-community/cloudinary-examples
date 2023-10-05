d1203f7ade69a05cb7020f3707e81e87a3bd6d71
s vamshi <somsolevamshi@gmail.com>
2023-10-05 22:30:23 +0530

    python-image-overlay

---
+++ examples/python-image-overlay/imageOverlay.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import cloudinary
+from cloudinary import CloudinaryImage
+
+config = cloudinary.config(secure=True)
+
+def imageOverlay():
+
+  image = CloudinaryImage("samples/smile.jpg").image(transformation=[
+  {
+    'width': 700, 
+    'height': 850, 
+    'crop': 'fill',
+  },
+  {
+    'overlay': "samples:breakfast",
+    'width': 200,
+    'height': 200,
+  },
+  {
+      'flags': 'layer_apply',
+      'gravity': 'north_east',
+      'x': 400,
+      'y': 400
+  }
+  ])
+  print(image)
+
+imageOverlay()
