d0d2a6dce885bcc36cedf4cc67ecae8fadc122e6
Syed Talal Jilani <72516408+syedtalaljilani@users.noreply.github.com>
2023-10-12 23:38:36 +0500

    php image upload code is added

--- examples/php-image-upload/imageupload.php
+++ examples/php-image-upload/imageupload.php
@@ -1,13 +1,16 @@
 <?php
-
-// Include the Cloudinary PHP SDK
 require 'vendor/autoload.php';
+use Cloudinary\Cloudinary;
+
+// Load environment variables from .env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 // Set your Cloudinary API credentials
-$cloudinary = new Cloudinary\Cloudinary([
-    'cloud_name' => 'dlzz0d4yw',
-    'api_key' => '375628352888284',
-    'api_secret' => 'kTg_FC7crxiXD65hgEm8YHSfe5I',
+$cloudinary = new Cloudinary([
+    'cloud_name' => $_ENV['CLOUDINARY_CLOUD_NAME'],
+    'api_key' => $_ENV['CLOUDINARY_API_KEY'],
+    'api_secret' => $_ENV['CLOUDINARY_API_SECRET'],
 ]);
 
 // Get the path to the image file to upload
@@ -17,9 +20,6 @@ $imagePath = 'demo.jpg';
 $uploadResult = $cloudinary->uploadApi()->upload($imagePath);
 
 if(isset($uploadResult)){
-    echo 'image upload';
+    echo 'Image uploaded';
 }
-
 ?>
-
-
