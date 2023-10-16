25bde08e04fe6133892aaa7ebebeb67376e3dc70
Colby Fayock <fayock@gmail.com>
2023-10-16 15:49:52 -0400

    updating script config

--- examples/php-transformations-effects/image-effects.php
+++ examples/php-transformations-effects/image-effects.php
@@ -11,8 +11,8 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $cld = new Cloudinary([
-  'cloud_name' => $_ENV["CLOUDINARY_CLOUD_NAME"], 
-  'api_key' => $_ENV["CLOUDINARY_API_KEY"], 
+  'cloud_name' => $_ENV["CLOUDINARY_CLOUD_NAME"],
+  'api_key' => $_ENV["CLOUDINARY_API_KEY"],
   'api_secret' => $_ENV["CLOUDINARY_API_SECRET"]
 ]);
 
--- examples/php-transformations-effects/image-overlay.php
+++ examples/php-transformations-effects/image-overlay.php
@@ -1,27 +1,23 @@
 #!/usr/bin/php
 <?php
-use Cloudinary\Configuration\Configuration;
-use Cloudinary\Tag\ImageTag;
-use Cloudinary\Transformation\TextStyle;
 require_once('vendor/autoload.php');
-use Cloudinary\Transformation\Effect;
-use Cloudinary\Transformation\Gravity;
-use Cloudinary\Transformation\ImageTransformation;
-use Cloudinary\Transformation\Rotate;
 
 use Cloudinary\Cloudinary;
+use Cloudinary\Configuration\Configuration;
+use Cloudinary\Tag\ImageTag;
 use Cloudinary\Transformation\Overlay;
-use Cloudinary\Transformation\Resize;
 use Cloudinary\Transformation\Source;
-          
 
-Configuration::instance([
-	'cloud_name' => $_ENV["CLOUDINARY_CLOUD_NAME"], 
-	'api_key' => $_ENV["CLOUDINARY_API_KEY"], 
-	'api_secret' => $_ENV["CLOUDINARY_API_SECRET"]
-  ]);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-echo ((new ImageTag('samples/smile.jpg')))
-->overlay(Overlay::source(
-	Source::fetch("https://res.cloudinary.com/demo/image/upload/logos/cloudinary_full_logo_white_small.png")));
+$cld = new Cloudinary([
+  'cloud_name' => $_ENV["CLOUDINARY_CLOUD_NAME"],
+  'api_key' => $_ENV["CLOUDINARY_API_KEY"],
+  'api_secret' => $_ENV["CLOUDINARY_API_SECRET"]
+]);
+
+echo $cld->image('samples/smile.jpg')
+	->overlay(Overlay::source(
+		Source::fetch("https://res.cloudinary.com/demo/image/upload/logos/cloudinary_full_logo_white_small.png")));
 ?>
