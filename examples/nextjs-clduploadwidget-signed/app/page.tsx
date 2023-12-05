21adc880c9c19de1e10464152538b40dd2316e7e
RichardBray <richardbray89@gmail.com>
2023-12-05 11:31:08 +0000

    feat: show uploaded image from signed CldUploadWidget

--- examples/nextjs-clduploadwidget-signed/app/page.tsx
+++ examples/nextjs-clduploadwidget-signed/app/page.tsx
@@ -1,8 +1,17 @@
 'use client';
 
-import { CldUploadWidget } from 'next-cloudinary';
+import { CldUploadWidget, CldImage } from 'next-cloudinary';
+import { useState } from 'react';
+
+interface UploadedAssetData {
+  public_id: string;
+  width: number;
+  height: number;
+  id: string;
+}
 
 export default function Home() {
+  const [result, setResult] = useState<UploadedAssetData | null>(null);
   return (
     <main className="p-16 text-center">
       <h1 className="text-5xl font-medium py-8">Image Upload App</h1>
@@ -15,13 +24,22 @@ export default function Home() {
         */}
 
         {/* SIGNED EXAMPLE */}
-        <CldUploadWidget signatureEndpoint="/api/sign-image">
+        <CldUploadWidget
+          signatureEndpoint="/api/sign-image"
+          onSuccess={(result) => {
+            setResult(result?.info as UploadedAssetData);
+          }}
+        >
           {({ open }) => (
             <button className="bg-indigo-500 rounded py-2 px-4 mb-4 text-white" onClick={() => open()}>
               Upload an Image
             </button>
           )}
         </CldUploadWidget>
+
+        {result ? (
+          <CldImage src={result.public_id} width={result.width} height={result.height} alt="Uploaded Image" />
+        ) : null}
       </section>
     </main>
   );
