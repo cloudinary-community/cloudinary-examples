b81723fa068194d254d5049e87e8fcfe224d6c78
Colby Fayock <fayock@gmail.com>
2024-03-06 14:30:34 -0500

    style tweaks

--- examples/nextjs-cldimage/pages/index.jsx
+++ examples/nextjs-cldimage/pages/index.jsx
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 import { CldImage } from 'next-cloudinary';
 import images from '../images.json';
-import { Poppins } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
-const poppins = Poppins({
+const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
   weight: ['400', '700'],
@@ -23,19 +23,27 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className={poppins.className}>
-        <header className='mb-12 mt-0 w-full bg-blue-950 px-0 py-4 text-center text-white'>
-          <h1 className='text-2xl font-semibold'>
-            <a
-              href='https://next.cloudinary.dev'
-              className='text-blue-200 underline-offset-2 hover:underline'
-              target='_blank'
-            >
-              Next Cloudinary&apos;s
-            </a>
-            <br />
-            <code className='font-mono'>{`<CldImage />`}</code> Component
-          </h1>
+      <main className={inter.className}>
+        <header className='mb-12 mt-0 w-full bg-blue-950 py-4 text-center text-white'>
+          <div className="flex justify-between flex-col sm:flex-row items-center max-w-screen-lg px-4 mx-auto">
+            <h1 className='flex gap-3 items-center text-xl font-semibold'>
+              <a
+                href='https://next.cloudinary.dev'
+                className='text-blue-200 underline-offset-2 hover:underline'
+                target='_blank'
+              >
+                Next Cloudinary
+              </a>
+              <code className='font-mono text-lg'>{`<CldImage />`}</code>
+            </h1>
+            <ul>
+              <li>
+                <a className="text-sm" href="https://github.com/cloudinary-community/cloudinary-examples/tree/main/examples/nextjs-cldimage">
+                  View on GitHub
+                </a>
+              </li>
+            </ul>
+          </div>
         </header>
 
         <div className='container mx-auto mb-5 max-w-screen-lg space-y-14 px-4 py-0'>
@@ -136,15 +144,17 @@ export default function Home() {
             })}
           </ImagesSection>
 
-          <div className='border-t'>
-            <h2 className='mt-5 text-3xl font-bold'>Resources</h2>
-            <a
-              href='https://github.com/colbyfayock/cloudinary-examples/tree/main/examples/nextjs-cldimage'
-              className='text-blue-800 hover:underline'
-              target='_blank'
-            >
-              See the code on github.com.
-            </a>
+          <div className='border-t pt-8'>
+            <h2 className='text-2xl font-bold mb-3'>Resources</h2>
+            <p className="mb-2">
+              <a
+                href='https://github.com/colbyfayock/cloudinary-examples/tree/main/examples/nextjs-cldimage'
+                className='text-blue-800 hover:underline'
+                target='_blank'
+              >
+                See the code on github.com.
+              </a>
+            </p>
             <p>
               Next Cloudinary:{' '}
               <a
@@ -165,7 +175,7 @@ export default function Home() {
 function ImagesSection({ title, children }) {
   return (
     <div>
-      <h2 className='mb-4 text-2xl font-semibold text-indigo-900'>{title}</h2>
+      <h2 className='mb-5 text-lg font-semibold text-indigo-900'>{title}</h2>
       <ul className='grid list-none grid-cols-1 gap-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
         {children}
       </ul>
