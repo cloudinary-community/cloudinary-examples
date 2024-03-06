eabfb54235951c265de04ea1a09a3f35f1789d84
Usman S. (Max Programming) <ui.sabuwala@gmail.com>
2024-03-07 00:53:05 +0300

    refactor: remove unused component

--- examples/nextjs-cldimage/components/CldImage.jsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { CldImage as CldImageDefault } from 'next-cloudinary';
-
-/** @param {import('next-cloudinary').CldImageProps} props */
-export default function CldImage(props) {
-  return <CldImageDefault {...props} />;
-}
