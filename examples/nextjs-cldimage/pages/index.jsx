41226895283765636487a82489a4f652340046c6
Usman S. (Max Programming) <ui.sabuwala@gmail.com>
2024-03-21 09:40:37 +0530

    refactor: remove inter font because it already exists in _app

--- examples/nextjs-cldimage/pages/index.jsx
+++ examples/nextjs-cldimage/pages/index.jsx
@@ -1,14 +1,7 @@
 import Head from 'next/head';
 import { CldImage } from 'next-cloudinary';
 import images from '../images.json';
-import { Inter } from 'next/font/google';
 import ImagesGrid from '../components/ImagesGrid';
-
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['400', '700'],
-});
 
 const TITLE = 'Next Cloudinary CldImage Component';
 
@@ -24,121 +17,119 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className={inter.className}>
-        <div className='container mx-auto mb-5 max-w-screen-lg space-y-14 px-4 py-0'>
-          <ImagesGrid title='Using Cloudinary Public IDs'>
-            {images.map((image) => (
-              <li key={image.id}>
-                <CldImage
-                  width={image.width}
-                  height={image.height}
-                  src={image.image}
-                  alt={image.title}
-                  sizes='(max-width: 640px) 100vw,
+      <div className='container mx-auto mb-5 max-w-screen-lg space-y-14 px-4 py-0'>
+        <ImagesGrid title='Using Cloudinary Public IDs'>
+          {images.map((image) => (
+            <li key={image.id}>
+              <CldImage
+                width={image.width}
+                height={image.height}
+                src={image.image}
+                alt={image.title}
+                sizes='(max-width: 640px) 100vw,
                       (max-width: 768px) 80vw,
                       (max-width: 1024px) 60vw,
                       50vw'
-                />
-              </li>
-            ))}
-          </ImagesGrid>
+              />
+            </li>
+          ))}
+        </ImagesGrid>
 
-          <ImagesGrid title='AI Generative Fill'>
-            {images.map((image) => (
-              <li key={image.id}>
-                <CldImage
-                  width={image.height}
-                  height={image.width}
-                  crop='pad'
-                  src={image.image}
-                  alt={image.title}
-                  fillBackground
-                  sizes='(max-width: 640px) 100vw,
+        <ImagesGrid title='AI Generative Fill'>
+          {images.map((image) => (
+            <li key={image.id}>
+              <CldImage
+                width={image.height}
+                height={image.width}
+                crop='pad'
+                src={image.image}
+                alt={image.title}
+                fillBackground
+                sizes='(max-width: 640px) 100vw,
                       (max-width: 768px) 80vw,
                       (max-width: 1024px) 60vw,
                       50vw'
-                />
-              </li>
-            ))}
-          </ImagesGrid>
+              />
+            </li>
+          ))}
+        </ImagesGrid>
 
-          <ImagesGrid title='Uniform Cropping'>
-            {images.map((image) => (
-              <li key={image.id}>
-                <CldImage
-                  width='600'
-                  height='600'
-                  crop='fill'
-                  src={image.image}
-                  alt={image.title}
-                  sizes='(max-width: 640px) 100vw,
+        <ImagesGrid title='Uniform Cropping'>
+          {images.map((image) => (
+            <li key={image.id}>
+              <CldImage
+                width='600'
+                height='600'
+                crop='fill'
+                src={image.image}
+                alt={image.title}
+                sizes='(max-width: 640px) 100vw,
                       (max-width: 768px) 80vw,
                       (max-width: 1024px) 60vw,
                       50vw'
-                />
-              </li>
-            ))}
-          </ImagesGrid>
+              />
+            </li>
+          ))}
+        </ImagesGrid>
 
-          <ImagesGrid title='With Effects'>
-            {images.map((image) => (
-              <li key={image.id}>
-                <CldImage
-                  width='600'
-                  height='600'
-                  crop={{
-                    crop: 'fill',
-                    source: true,
-                  }}
-                  src={image.image}
-                  alt={image.title}
-                  tint='equalize:80:blue:blueviolet'
-                  blur='1500'
-                  overlays={[
-                    {
-                      text: {
-                        color: 'white',
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 100,
-                        fontWeight: 'bold',
-                        letterSpacing: 14,
-                        text: image.title,
-                      },
+        <ImagesGrid title='With Effects'>
+          {images.map((image) => (
+            <li key={image.id}>
+              <CldImage
+                width='600'
+                height='600'
+                crop={{
+                  crop: 'fill',
+                  source: true,
+                }}
+                src={image.image}
+                alt={image.title}
+                tint='equalize:80:blue:blueviolet'
+                blur='1500'
+                overlays={[
+                  {
+                    text: {
+                      color: 'white',
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 100,
+                      fontWeight: 'bold',
+                      letterSpacing: 14,
+                      text: image.title,
                     },
-                  ]}
-                  sizes='(max-width: 640px) 100vw,
+                  },
+                ]}
+                sizes='(max-width: 640px) 100vw,
                       (max-width: 768px) 80vw,
                       (max-width: 1024px) 60vw,
                       50vw'
-                />
-              </li>
-            ))}
-          </ImagesGrid>
+              />
+            </li>
+          ))}
+        </ImagesGrid>
 
-          <div className='border-t pt-8'>
-            <h2 className='mb-3 text-2xl font-bold'>Resources</h2>
-            <p className='mb-2'>
-              <a
-                href='https://github.com/colbyfayock/cloudinary-examples/tree/main/examples/nextjs-cldimage'
-                className='text-blue-800 hover:underline'
-                target='_blank'
-              >
-                See the code on github.com.
-              </a>
-            </p>
-            <p>
-              Next Cloudinary:{' '}
-              <a
-                href='https://next.cloudinary.dev/'
-                className='text-blue-800 hover:underline'
-                target='_blank'
-              >
-                https://next.cloudinary.dev
-              </a>
-            </p>
-          </div>
+        <div className='border-t pt-8'>
+          <h2 className='mb-3 text-2xl font-bold'>Resources</h2>
+          <p className='mb-2'>
+            <a
+              href='https://github.com/colbyfayock/cloudinary-examples/tree/main/examples/nextjs-cldimage'
+              className='text-blue-800 hover:underline'
+              target='_blank'
+            >
+              See the code on github.com.
+            </a>
+          </p>
+          <p>
+            Next Cloudinary:{' '}
+            <a
+              href='https://next.cloudinary.dev/'
+              className='text-blue-800 hover:underline'
+              target='_blank'
+            >
+              https://next.cloudinary.dev
+            </a>
+          </p>
         </div>
-      </main>
+      </div>
     </>
   );
 }
