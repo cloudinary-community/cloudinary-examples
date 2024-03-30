45f45ea3f18a1416bbb0c46e0101e52292958a35
Usman S <ui.sabuwala@gmail.com>
2024-03-30 22:39:06 +0530

    Use next config mjs

--- examples/nextjs-cldogimage/next.config.mjs
+++ examples/nextjs-cldogimage/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['res.cloudinary.com']
-  }
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
