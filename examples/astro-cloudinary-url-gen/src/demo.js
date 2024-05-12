3376532c2d34f1a3637cb8d3258f3dc70c160c29
Dillion Megida <dillionmegida@gmail.com>
2024-05-12 23:21:38 +0200

    add astro image demo

---
+++ examples/astro-cloudinary-url-gen/src/demo.js
@@ -0,0 +1,53 @@
+export const products = [{
+  public_id: "astro/phone-1",
+  unsplash: "https://images.unsplash.com/photo-1586941756923-830029962fd9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODd8fHBob25lc3xlbnwwfHwwfHx8MA%3D%3D",
+  alt: "An iPhone on someone's palms"
+},{
+  public_id: "astro/phone-2",
+  unsplash: "https://images.unsplash.com/photo-1598327106026-d9521da673d1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDN8fHBob25lc3xlbnwwfHwwfHx8MA%3D%3D",
+  alt: "A legacy button phone"
+},
+{
+  public_id: "astro/phone-3",
+  unsplash: "https://images.unsplash.com/photo-1543069190-9d380c458bc2?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fHBob25lc3xlbnwwfHwwfHx8MA%3D%3D",
+alt: "A close up of a mobile phone's home screen"}
+, {
+  public_id: "astro/phone-4",
+  unsplash: "https://images.unsplash.com/photo-1597740985671-2a8a3b80502e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBob25lc3xlbnwwfHwwfHx8MA%3D%3D",
+  alt: "A hand holding a flip android phone"
+}, {
+  public_id: "astro/phone-5",
+  unsplash: "https://images.unsplash.com/photo-1569183091671-696402586b9c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBob25lc3xlbnwwfHwwfHx8MA%3D%3D",
+  alt: "An android phone on a table"
+}, {
+  public_id: "astro/phone-6",
+  unsplash: "https://images.unsplash.com/photo-1621330396173-e41b1cafd17f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGhvbmVzfGVufDB8fDB8fHww",
+  alt: "An iPhone floating in the air"
+}, {
+  public_id: "astro/phone-7",
+  unsplash: "https://images.unsplash.com/photo-1592813630413-1124aa567638?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHBob25lc3xlbnwwfHwwfHx8MA%3D%3D",
+  alt: "Two android phones side by side"
+}, {
+  public_id: "astro/phone-8",
+  unsplash: "https://images.unsplash.com/photo-1574547070681-291aeb4b2485?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHBob25lc3xlbnwwfHwwfHx8MA%3D%3D",
+  alt: "A hand holding an iPhone"
+}]
+
+export const people = [{
+  public_id: "astro/person-1",
+  unsplash: "https://images.unsplash.com/photo-1500048993953-d23a436266cf?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+  alt: "A close up of a guy smiling while leaning on a wall"
+},{
+  public_id: "astro/person-2",
+  unsplash: "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDR8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
+  alt: "A close up of guy on glass smiling"
+},
+{
+  public_id: "astro/person-3",
+  unsplash: "https://images.unsplash.com/photo-1495603889488-42d1d66e5523?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+alt: "A guy on suit standing with a pose"}
+, {
+  public_id: "astro/person-4",
+  unsplash: "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGJsYWNrJTIwcGVyc29ufGVufDB8fDB8fHww",
+  alt: "A lady smiling and pointing to her right direction"
+}]
