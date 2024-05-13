4b581267495880a15b8a3e9aee111ab528f84a5a
Colby Fayock <fayock@gmail.com>
2024-05-13 08:45:45 -0400

    updating env variables

--- examples/react-drag-drop-upload/src/App.jsx
+++ examples/react-drag-drop-upload/src/App.jsx
@@ -38,10 +38,10 @@ export default function MyDropzone() {
     let formData = new FormData()
 
     formData.append("file", selectedFile)
-    formData.append("upload_preset", "blok1oi1")
+    formData.append("upload_preset", import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET)
     formData.append("api_key", import.meta.env.VITE_CLOUDINARY_API_KEY)
 
-    await fetch(import.meta.env.VITE_CLOUDINARY_API, {
+    await fetch(`https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`, {
       method: "POST",
       body: formData,
     })
