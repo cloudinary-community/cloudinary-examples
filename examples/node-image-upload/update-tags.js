a5c891b8671fbbcd42d37fbfb78e2894061a5734
Colby Fayock <fayock@gmail.com>
2024-06-18 23:00:21 -0400

    tweaks

--- examples/node-image-upload/update-tags.js
+++ examples/node-image-upload/update-tags.js
@@ -1,40 +1,43 @@
 require('dotenv').config()
 const cloudinary = require('cloudinary').v2;
+const pLimit = require('p-limit');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-})
-
-const logTags = response => {
-    const appliedTags = response.tags
-    const possibleTags = response.info.categorization.google_tagging.data.slice(
-        0,
-        10,
-    )
-
-    console.log("POSSIBLE TAGS")
-    console.table(possibleTags)
-
-    console.log("APPLIED TAGS")
-    console.table(appliedTags)
-}
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const images = [
-    "aldebaran-s-uXchDIKs4qI-unsplash.jpg",
-    "brian-mcgowan-I0fDR8xtApA-unsplash.jpg",
-    "spacex-OHOU-5UVIYQ-unsplash.jpg",
-    "spacex-VBNb52J8Trk-unsplash.jpg",
-    "stellan-johansson-1PP0Fc-KSd4-unsplash.jpg",
-]
+  // this needs to be an active public ID in your configured Cloudinary
+  // account. Tip: use upload-multiple.js and copy the resulting IDs
+  '<Public ID>',
+];
 
-const promises = images.map(image =>
-    cloudinary.api.update(`cloudinary-existing-image-tag-demo/${image}`, {
+(async function run() {
+  const limit = pLimit(2);
+
+  const imagesToUpdate = images.map((image) => {
+    return limit(async () => {
+
+      // Use the update method passing along the categorization and auto_tagging configuration
+
+      const result = await cloudinary.api.update(image, {
         categorization: "google_tagging",
         auto_tagging: 0.9,
-    }),
-)
+      });
 
-const results = await Promise.all(promises)
-results.forEach(result => logTags(result))
+      console.log(`Successfully updated ${image}`);
+
+      console.log('> POSSIBLE TAGS');
+      console.table(result.info.categorization.google_tagging.data.slice(0, 10));
+
+      console.log('> APPLIED TAGS');
+      console.table(result.tags);
+
+      return result;
+    })
+  });
+
+  await Promise.all(imagesToUpdate);
+})();
