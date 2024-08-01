b049dc0fe9fb8e2a6bce991ce82db695cc414a43
Usman S. (Max Programming) <51731966+max-programming@users.noreply.github.com>
2024-08-01 18:47:32 +0530

    Use common Uploader component in pages router

--- examples/nextjs-clduploadwidget-captions/pages/index.tsx
+++ examples/nextjs-clduploadwidget-captions/pages/index.tsx
@@ -1,11 +1,8 @@
 import Footer from '@/components/Footer';
-import {
-  CldUploadWidget,
-  CldImage,
-  type CloudinaryUploadWidgetInfo,
-} from 'next-cloudinary';
+import Uploader from '@/components/Uploader';
 import Head from 'next/head';
 import { useState } from 'react';
+import type { CloudinaryUploadWidgetInfo } from 'next-cloudinary';
 
 export default function HomePage() {
   const [result, setResult] = useState<CloudinaryUploadWidgetInfo>();
@@ -30,50 +27,7 @@ export default function HomePage() {
             AI Content Analysis
           </a>
         </h2>
-        <section className="flex flex-col items-center justify-between">
-          <CldUploadWidget
-            signatureEndpoint="/api/sign-image"
-            options={{
-              detection: 'captioning',
-              on_success:
-                'current_asset.update({context: {alt: e.upload_info?.info?.detection?.captioning?.data?.caption}})',
-            }}
-            onSuccess={(result) => {
-              if (typeof result.info === 'string') return;
-              setResult(result.info);
-            }}
-          >
-            {({ open }) => (
-              <button
-                className="me-2 inline-flex items-center rounded-lg bg-indigo-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-indigo-300"
-                onClick={() => open()}
-                type="button"
-              >
-                Upload an image
-              </button>
-            )}
-          </CldUploadWidget>
-
-          {result ? (
-            <>
-              <blockquote className="relative border-s-4 border-indigo-700 ps-4 sm:ps-6 my-12">
-                <p className="text-neutral-800 sm:text-lg">
-                  Caption:{' '}
-                  <em className="font-bold text-indigo-800">
-                    {result.context.custom.alt}
-                  </em>
-                </p>
-              </blockquote>
-              <CldImage
-                src={result.public_id}
-                width={result.width}
-                height={result.height}
-                alt={result.context.custom.alt}
-                title={result.context.custom.alt}
-              />
-            </>
-          ) : null}
-        </section>
+        <Uploader />
         <Footer />
       </div>
     </>
