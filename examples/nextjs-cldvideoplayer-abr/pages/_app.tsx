94f89098eb71ee0a697b0106d2ac46efa611e503
Usman S. <ui.sabuwala@gmail.com>
2024-08-03 13:30:50 +0530

    Pages router example

---
+++ examples/nextjs-cldvideoplayer-abr/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import type { AppProps } from "next/app";
+
+import "next-cloudinary/dist/cld-video-player.css";
+import "@/styles/globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "700"],
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={inter.className}>
+      <Header />
+      <Component {...pageProps} />
+    </main>
+  );
+}
