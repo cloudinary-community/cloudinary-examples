78bdb306997c82869190fef56d63d9e325b83ce8
Usman S. <ui.sabuwala@gmail.com>
2024-08-03 13:29:45 +0530

    Upgrade dependencies and minor adjustments

--- examples/nextjs-cldvideoplayer-abr/tailwind.config.ts
+++ examples/nextjs-cldvideoplayer-abr/tailwind.config.ts
@@ -1,20 +1,15 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
-export default config
+} satisfies Config;
+
+export default config;
