c078792b3145ae4f60587a3de5d25b1dc1d2ba8b
Usman S. <ui.sabuwala@gmail.com>
2024-08-15 01:02:41 +0530

    Reusable components

---
+++ examples/nextjs-next-image-domain/components/Footer.jsx
@@ -0,0 +1,26 @@
+export default function Footer() {
+  return (
+    <div className="border-t pt-8">
+      <h2 className="mb-3 text-2xl font-bold">Resources</h2>
+      <p className="mb-2">
+        <a
+          href="https://github.com/colbyfayock/cloudinary-examples/tree/main/examples/nextjs-next-image-domain"
+          className="text-blue-800 hover:underline"
+          target="_blank"
+        >
+          See the code on github.com.
+        </a>
+      </p>
+      <p>
+        Next Cloudinary:{" "}
+        <a
+          href="https://next.cloudinary.dev/"
+          className="text-blue-800 hover:underline"
+          target="_blank"
+        >
+          https://next.cloudinary.dev
+        </a>
+      </p>
+    </div>
+  );
+}
