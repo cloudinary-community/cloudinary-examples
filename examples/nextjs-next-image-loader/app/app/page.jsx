e4d6bd563632e7e8a56c455e7816be8a083d0755
Usman S. <ui.sabuwala@gmail.com>
2024-08-15 00:52:03 +0530

    Update note

--- examples/nextjs-next-image-loader/app/app/page.jsx
+++ examples/nextjs-next-image-loader/app/app/page.jsx
@@ -4,14 +4,19 @@ import Footer from "../../components/Footer";
 
 import images from "../../images.json";
 
+export const metadata = {
+  title: "Next.js Image Component & Cloudinary - App Router Demo",
+  description:
+    "Find more Cloudinary examples at github.com/cloudinary-community/cloudinary-examples",
+};
+
 export default function AppDemoPage() {
   return (
     <div className="container mx-auto mb-5 max-w-screen-lg space-y-5 px-4 py-0">
       <h2 className="text-2xl font-semibold text-indigo-900">Example</h2>
       <blockquote className="relative border-s-4 ps-4 sm:ps-6 dark:border-gray-700">
-        Warning: This is not recommended because you have to manually add
-        customization options to the <code>src</code> url. The best approach is
-        to use the{" "}
+        ⚠️ Note: This is not the recommended approach. Use the CldImage
+        component with{" "}
         <a
           className="text-blue-800 hover:underline"
           href="https://next.cloudinary.dev"
@@ -19,7 +24,7 @@ export default function AppDemoPage() {
         >
           Next Cloudinary
         </a>{" "}
-        library.
+        for first-class support of Cloudinary with the Next.js Image component.
       </blockquote>
       <ImagesGrid>
         {images.map(image => {
--- examples/nextjs-next-image-loader/pages/index.jsx
+++ examples/nextjs-next-image-loader/pages/index.jsx
@@ -24,9 +24,8 @@ export default function Home() {
       <div className="container mx-auto mb-5 max-w-screen-lg space-y-5 px-4 py-0">
         <h2 className="text-2xl font-semibold text-indigo-900">Example</h2>
         <blockquote className="relative border-s-4 ps-4 sm:ps-6 dark:border-gray-700">
-          Warning: This is not recommended because you have to manually add
-          customization options to the <code>src</code> url. The best approach
-          is to use the{" "}
+          ⚠️ Note: This is not the recommended approach. Use the CldImage
+          component with{" "}
           <a
             className="text-blue-800 hover:underline"
             href="https://next.cloudinary.dev"
@@ -34,7 +33,8 @@ export default function Home() {
           >
             Next Cloudinary
           </a>{" "}
-          library.
+          for first-class support of Cloudinary with the Next.js Image
+          component.
         </blockquote>
         <ImagesGrid>
           {images.map(image => {
