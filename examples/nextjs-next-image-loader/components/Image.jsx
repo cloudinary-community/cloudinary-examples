48ce72a803deca5ac33d0447e2de18998b9efae4
Usman S. <ui.sabuwala@gmail.com>
2024-08-15 00:41:50 +0530

    minor update

--- examples/nextjs-next-image-loader/components/Image.jsx
+++ examples/nextjs-next-image-loader/components/Image.jsx
@@ -2,7 +2,7 @@
 import NextImage from "next/image";
 import { cloudinaryLoader } from "../utils/cloudinaryLoader";
 
-/** @param {*import('next/image').ImageProps} props  */
+/** @param {import('next/image').ImageProps} props  */
 export default function Image(props) {
   return <NextImage {...props} loader={cloudinaryLoader} />;
 }
