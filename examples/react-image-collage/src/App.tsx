f0d92d6b5907c0d11a4777fc9bd636424db0a2f2
Colby Fayock <fayock@gmail.com>
2024-08-26 11:11:56 -0400

    updating references to use sample images

--- examples/react-image-collage/src/App.tsx
+++ examples/react-image-collage/src/App.tsx
@@ -4,22 +4,26 @@ import { source } from '@cloudinary/url-gen/actions/overlay';
 import { image } from '@cloudinary/url-gen/qualifiers/source';
 import { Position } from '@cloudinary/url-gen/qualifiers/position';
 import { autoGravity, compass } from '@cloudinary/url-gen/qualifiers/gravity';
-import { AdvancedImage } from '@cloudinary/react';
 import { colorize } from '@cloudinary/url-gen/actions/effect';
+import { AdvancedImage } from '@cloudinary/react';
 
 function App() {
   const cld = new Cloudinary({
     cloud: {
-      cloudName: "cloud_name"
+      cloudName: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
     }
   });
 
-  const baseImg = cld.image("flying_phone")
+  /**
+   * Option 1: Manually creating overlays for each location
+   */
+
+  const baseImg = cld.image("samples/dessert-on-a-plate")
     .resize(fill().width(1200).height(1200))
     .effect(colorize().color('white'));
 
   baseImg.overlay(
-        source(image("flying_phone")
+        source(image("samples/dessert-on-a-plate")
           .transformation(
             new Transformation().resize(
               fill().width(600).height(1200).gravity(autoGravity())
@@ -32,7 +36,7 @@ function App() {
   );
 
   baseImg.overlay(
-        source(image("paintbrushes")
+        source(image("cld-sample-4")
           .transformation(
             new Transformation().resize(
               fill().width(600).height(1200).gravity(autoGravity())
@@ -45,7 +49,7 @@ function App() {
   );
 
   baseImg.overlay(
-        source(image("hiking")
+        source(image("samples/breakfast")
           .transformation(
             new Transformation().resize(
               fill().width(600).height(600).gravity(autoGravity())
@@ -57,63 +61,55 @@ function App() {
     )
   );
 
+  /**
+   * Option 2: Creating a reusable function for abstracting overlay generation
+   */
+
+  const baseImg2 = cld.image('samples/dessert-on-a-plate').resize(fill().width(1200).height(1200)).effect(colorize().color('white'));
+
+  const addOverlays = (overlays: Overlay[]) => {
+    overlays.forEach((overlay: Overlay) => {
+      const { imageName, width, height, gravity, offsetX } = overlay;
+      baseImg2.overlay(
+        source(
+          image(imageName).transformation(
+            new Transformation().resize(fill().width(width).height(height).gravity(autoGravity()))
+          )
+        ).position(new Position().gravity(compass(gravity)).offsetX(offsetX ?? 0))
+
+      );
+    });
+  };
+
+  type Overlay = {
+    imageName: string;
+    width: number;
+    height: number;
+    gravity: string,
+    offsetX?: number,
+  };
+
+  const overlays: Overlay[] = [
+    { imageName: 'samples/dessert-on-a-plate', width: 300, height: 600, gravity: 'north_west' },
+    { imageName: 'cld-sample-4', width: 300, height: 600, gravity: 'north_west', offsetX: 300 },
+    { imageName: 'samples/breakfast', width: 600, height: 600, gravity: 'south_west' },
+    { imageName: 'samples/food/pot-mussels', width: 600, height: 600, gravity: 'north_east' },
+    { imageName: 'samples/food/spices', width: 600, height: 600, gravity: 'south_east' },
+  ];
+
+  addOverlays(overlays);
+
   return (
     <>
-      <AdvancedImage cldImg={baseImg} />
+      <h2>Basic Collage</h2>
+      
+      <AdvancedImage cldImg={baseImg} width={600} height={600} />
+      
+      <h2>Collage with Generator Function</h2>
+      
+      <AdvancedImage cldImg={baseImg2} width={600} height={600} />
     </>
   );
 }
 
 export default App;
-
-// Code for abstracting into a function
-
-// function App() {
-//   const cld = new Cloudinary({
-//     cloud: {
-//       cloudName: 'demunxdyd',
-//     },
-//   });
-
-//   const baseImg = cld.image('flying_phone').resize(fill().width(1200).height(1200)).effect(colorize().color('white'));
-
-//   const addOverlays = (overlays: Overlay[]) => {
-//     overlays.forEach((overlay: Overlay) => {
-//       const { imageName, width, height, gravity, offsetX } = overlay;
-//       baseImg.overlay(
-//         source(
-//           image(imageName).transformation(
-//             new Transformation().resize(fill().width(width).height(height).gravity(autoGravity()))
-//           )
-//         ).position(new Position().gravity(compass(gravity)).offsetX(offsetX ?? 0))
-
-//       );
-//     });
-//   };
-
-//   type Overlay = {
-//     imageName: string;
-//     width: number;
-//     height: number;
-//     gravity: string,
-//     offsetX?: number,
-//   };
-
-//   const overlays: Overlay[] = [
-//     { imageName: 'food_3_lwvupk', width: 300, height: 600, gravity: 'north_west' },
-//     { imageName: 'cld-sample-4', width: 300, height: 600, gravity: 'north_west', offsetX: 300 },
-//     { imageName: 'food_1_bnjenq', width: 600, height: 600, gravity: 'south_west' },
-//     { imageName: 'food_4_sbgsu3', width: 600, height: 600, gravity: 'north_east' },
-//     { imageName: 'food_2_s10f9o', width: 600, height: 600, gravity: 'south_east' },
-//   ];
-
-//   addOverlays(overlays);
-
-//   return (
-//     <>
-//       <AdvancedImage cldImg={baseImg} />
-//     </>
-//   );
-// }
-
-// export default App;
