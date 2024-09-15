6111ce3c707e6017130c596a14f0044338869a3c
Usman S. <ui.sabuwala@gmail.com>
2024-09-15 12:12:27 +0530

    Add a check for env variable

--- examples/nextjs-upload-formdata/app/app/api/sign/route.ts
+++ examples/nextjs-upload-formdata/app/app/api/sign/route.ts
@@ -1,6 +1,10 @@
 import cloudinary from "@/lib/cloudinary";
 import type { SignApiOptions } from "cloudinary";
 
+if (!process.env.CLOUDINARY_API_SECRET) {
+  throw new Error("Missing CLOUDINARY_API_SECRET");
+}
+
 export async function POST(req: Request) {
   const body: SignApiOptions = await req.json();
   const signature = cloudinary.utils.api_sign_request(
